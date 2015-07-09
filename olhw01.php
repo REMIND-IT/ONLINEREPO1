0e2f543fc0736531020755b27cc8b06967176d0e
REMIND-IT <development@remind-it.de>
2015-07-09 17:48:50 +0200

    v1.1
    in branch local after changes in file

--- olhw01.php
+++ olhw01.php
@@ -4,5 +4,6 @@
  </head>
  <body>
  <?php echo '<p>Hello World in ONLINEREPO1 V1.0 online</p><br>'; ?>
+ <?php echo '<p>Changed locally after cloning, syncing and creating a branch .. v1.1 local</p><br>'; ?>
  </body>
 </html>
